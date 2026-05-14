911581ac95626df92eabeab3e3217e5d8e08e222
KHASIM <khasim2586@gmail.com>
2026-05-14 23:08:46 +0530

    connected backend deployed url

--- WEEK-6/mini-mern-app/Frontend/vite-project/src/api/axiosInstance.js
+++ WEEK-6/mini-mern-app/Frontend/vite-project/src/api/axiosInstance.js
@@ -4,8 +4,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // Use VITE_API_BASE_URL from env, or default to Render in production and localhost in dev
   baseURL: import.meta.env.VITE_API_BASE_URL || 
-          (import.meta.env.MODE === "production" ? "https://mern-mini-app-qksp.onrender.com" : "http://localhost:3000"),
-  withCredentials: true,
+          (import.meta.env.MODE === "production" ? "https://mern-mini-app-qksp.onrender.com" : "http://localhost:3000")
 });
 
 export default axiosInstance;
